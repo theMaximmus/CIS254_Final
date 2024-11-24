b7f56ec968de298292905f54d288b55437ea8f9c
theMaximmus <m.stesev@gmail.com>
2024-11-23 21:08:43 -0800

    fixed listView!

--- src/main/java/com/maximmus/cis254_final/MailController.java
+++ src/main/java/com/maximmus/cis254_final/MailController.java
@@ -1,5 +1,7 @@
 package com.maximmus.cis254_final;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Label;
@@ -11,15 +13,16 @@ import java.util.ResourceBundle;
 public class MailController implements Initializable {
     @FXML
     private Label welcomeText;
+    ObservableList<String> names = FXCollections.observableArrayList(
+            "Julia", "Ian", "Sue", "Matthew", "Hannah", "Stephan", "Denise");
+    @FXML
+    private ListView<String> draftListView = new ListView<>();
 
     @FXML
-    private ListView<String> draftListView;
+    private ListView<String> inboxListView = new ListView<>();
 
     @FXML
-    private ListView<String> inboxListView;
-
-    @FXML
-    private ListView<String> sentListView;
+    private ListView<String> sentListView = new ListView<>();
 
     @FXML
     protected void onHelloButtonClick() {
@@ -30,5 +33,6 @@ public class MailController implements Initializable {
     public void initialize(URL url, ResourceBundle resourceBundle) {
         String[] list = {"Java", "SOSI", "MB?????"};
         inboxListView.getItems().addAll(list);
+        System.out.println(inboxListView.getItems().size());
     }
 }
