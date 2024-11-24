aa3bc2389d6e51ec70d8b65065a6d0046c0d24fb
theMaximmus <m.stesev@gmail.com>
2024-11-24 11:14:46 -0800

    added preliminary layout; implemented user icon and added an event handler that displays a new whole pop-up window that is an instance of another JavaFx class.

--- src/main/java/com/maximmus/cis254_final/MailApplication.java
+++ src/main/java/com/maximmus/cis254_final/MailApplication.java
@@ -21,4 +21,5 @@ public class MailApplication extends Application {
     public static void main(String[] args) {
         launch();
     }
+
 }
--- src/main/java/com/maximmus/cis254_final/MailController.java
+++ src/main/java/com/maximmus/cis254_final/MailController.java
@@ -1,18 +1,27 @@
 package com.maximmus.cis254_final;
 
+import com.maximmus.cis254_final.RegistrationWindow.RegistrationWindow;
+import javafx.application.Application;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Label;
 import javafx.scene.control.ListView;
+import javafx.scene.image.Image;
+import javafx.scene.paint.ImagePattern;
+import javafx.scene.shape.Circle;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
 public class MailController implements Initializable {
     @FXML
     private Label welcomeText;
+    @FXML
+    private Circle userIconCircle;
     @FXML
     private ListView<String> draftListView = new ListView<>();
 
@@ -34,5 +43,15 @@ public class MailController implements Initializable {
         draftListView.getItems().addAll(list);
         sentListView.getItems().addAll(list);
         System.out.println(inboxListView.getItems().size());
+
+        // adding user icon
+        Image image = new Image(getClass().getResourceAsStream("/com/maximmus/cis254_final/user_icon.png"));
+        userIconCircle.setFill(new ImagePattern(image));
+
+        // Handle the click event on the circle
+        userIconCircle.setOnMouseClicked(event -> {
+            RegistrationWindow registrationWindow = new RegistrationWindow();
+            registrationWindow.show();
+        });
     }
 }
